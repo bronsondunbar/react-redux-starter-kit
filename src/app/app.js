da0b32f9d51a013cbeea452f90b4df6402a4f235
Bronson Dunbar <bronson@bronsondunbar.com>
2018-07-25 09:38:02 +0200

    Setup commit

---
+++ src/app/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+export default class App extends Component {
+  render() {
+    return (
+    	<div className="uk-container">
+    		<h1 className="uk-heading-line uk-heading-primary uk-text-center uk-padding">
+    			<span>React Redux Starter</span>
+    		</h1>
+    	</div>
+    )
+  }
+}
